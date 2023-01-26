633ac799f193fed3b2972a2e73394f3a72b6661d
Worth Payton <49085523+worthpayton@users.noreply.github.com>
2023-01-25 20:23:21 -0600

    file and foler structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CssBaseline } from '@mui/material';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    App
-  </div>
-);
+import useStyles from './styles';
+
+// eslint-disable-next-line import/no-useless-path-segments
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/no-named-as-default
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
